f535d1526dc45ee1cecc27924776b27a3ee94617
David J Sealey <david.sealey@soak.co.uk>
2020-11-08 11:48:37 +0000

    Feature: Display words

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Word = () => {
+const Word = ({ selectedWord, correctLetters }) => {
   return (
-    <div className="word" id="word"></div>
+    <div className="word">
+      {selectedWord.split('').map((letter, index) => (
+          <span className="letter" key={index}>
+            {correctLetters.includes(letter) ? letter : ''}
+          </span>
+        )
+      )}
+    </div>
   )
 }
 
