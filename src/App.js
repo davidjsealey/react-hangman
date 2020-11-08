36cc056ee66477b1b4aaeae843e7bec5bd173175
David J Sealey <david.sealey@soak.co.uk>
2020-11-08 11:19:02 +0000

    Feature: Figure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Header from './components/Header';
+import Figure from './components/Figure';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </>
   );
 }
