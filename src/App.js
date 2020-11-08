1f1554ed0e2599da00c645668573589868570e52
David J Sealey <david.sealey@soak.co.uk>
2020-11-08 11:24:07 +0000

    Feature: Word

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="game-container">
         <Figure />
         <WrongLetters />
+        <Word />
       </div>
     </>
   );
