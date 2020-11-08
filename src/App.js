53969ceb24cdb80b2879f45fb2a8e8603e0791e3
David J Sealey <david.sealey@soak.co.uk>
2020-11-08 11:41:30 +0000

    Feature: Initial state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 import Header from './components/Header';
@@ -5,7 +6,14 @@ import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
